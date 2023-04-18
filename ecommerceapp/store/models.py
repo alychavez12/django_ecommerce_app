de693700d993b0ee4bc8eea90a9b54f9b674c211
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-04-18 16:23:02 -0400

    wip: creates a Product model

--- ecommerceapp/store/models.py
+++ ecommerceapp/store/models.py
@@ -11,3 +11,15 @@ class Category(models.Model):
     def __str__(self):
         return self.title
   
+class Product(models.Model):
+    user = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
