a772a1e51f22fbf8b5c50e7f588aa153df8f00ec
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-04-18 16:08:45 -0400

    wip: creates a model for categories

--- ecommerceapp/store/models.py
+++ ecommerceapp/store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True) # slug is a url human friendly version of the title
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
+  
