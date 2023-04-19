f506a3872c43559f1a28fea14d7832bd3f1bdabf
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-04-18 20:02:44 -0400

    wip: adds categories links working

--- ecommerceapp/store/views.py
+++ ecommerceapp/store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
-from .models import Product
+from .models import Category, Product
 
-def product_detail(request, slug):
+def category_detail(request, slug):
+    category = Category.objects.get(slug=slug)
+    products = category.products.all()
+    return render(request, 'store/category_detail.html',
+                   {'category': category,
+                    'products': products})
+
+def product_detail(request, category_slug, slug):
     product = Product.objects.get(slug=slug)
     return render(request, 'store/product_detail.html',
                    {'product': product})
