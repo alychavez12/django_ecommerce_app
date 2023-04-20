47df28f2e0bf481c03d317076ee7b29d430793b9
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-04-20 16:19:20 -0400

    feat: implement image uploading working

--- ecommerceapp/store/models.py
+++ ecommerceapp/store/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='uploads/product_images/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
